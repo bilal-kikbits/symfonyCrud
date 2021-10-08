82dfad1b483c5d0c7f8cd86ee306cfbc24e9e5fa
bilal-kikbits <bilal.majeed@kikbits.com>
2021-10-08 18:31:18 +0500

    updating changes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,15 +1,15 @@
 <?php
   namespace App\Controller;
   use App\Entity\Article;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-  use Symfony\Component\Form\Extension\Core\Type\TextType;
-  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
   class ArticleController extends Controller {
